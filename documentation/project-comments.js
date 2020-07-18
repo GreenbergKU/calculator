f932c5b6d3096e71b94371634906b6ba702adc09
Suzie Q <suzieqwolf@gmail.com>
2020-07-18 01:06:20 -0600

    Adds changes for completed calculator project.

---
+++ documentation/project-comments.js
@@ -0,0 +1,60 @@
+var CALCULATOR = PROJECT +odin 
+
+// function filterClick(event)
+
+// console.log("click event: ", event);
+// console.log('event.target.innerText: ', event.target.innerText);
+
+    //console.log("event.target.name: ", event.target.name)
+
+    // numMemory = Number(inputNum.value);
+    //     console.log('numMemory: ', numMemory, "typeof(numMemory): ", typeof(numMemory));
+    // inputNum.value = "";
+
+
+    // let numInput = Number(inputNum.value);
+    // numMemory = operate(opSymbol.name, numMemory, numInput);
+    // inputNum.value = numMemory;        
+    
+    //console.log('event.target.name: ', event.target.name);
+    //console.log('number: ', numInput);
+    //console.log('TYPEOF(opSymbol.name, numMemory, inputNum.value): ', typeof(opSymbol.name), typeof(numMemory), typeof(number));
+    //console.log('inputNum.value: ', inputNum.value); 
+  
+    // opSymbol.innerText === "" ? (        
+        //     displayOperator(event, opSymbol),
+        //     saveDisplayedValue(inputNum)
+        // ) : (
+        //     calculateValues(inputNum, opSymbol),
+        //     displayOperator(event, opSymbol)
+        // )
+ 
+
+
+   
+// function filterKeys(event) 
+    
+// console.log("key event: ", event);
+        // numMemory = new Number(inputNum.value);
+        //     console.log('numMemory: ', numMemory, "typeof(numMemory): ", typeof(numMemory));
+        // inputNum.value = "";
+        // let numInput = Number(inputNum.value);
+        // console.log('number: ', numInput);
+        // console.log('TYPEOF(opSymbol.name, numMemory, inputNum.value): ', typeof(opSymbol.name), typeof(numMemory), typeof(number));
+        // numMemory = operate(opSymbol.name, numMemory, numInput);
+        // inputNum.value = numMemory;
+        // console.log('inputNum.value: ', inputNum.value); 
+
+
+// function displayOperator(event, element) 
+    // console.log("event.key: ", event.key),
+    // console.log(event.target.name),
+    // console.log("event.key: ", event.key),
+    // console.log('element.name: ', element.name);
+
+
+// function displayNumbers(event, element)    
+    // let inputNum = document.querySelector("#number-input");
+    // displayValue(inputNumValue);
+
+// console.log('numMemory: ', numMemory, "typeof(numMemory): ", typeof(numMemory));
--- src/main.js
+++ src/main.js
@@ -1,3 +1,102 @@
+let calSec = document.querySelector("#cal-sec");
+
+let numMemory = 0;
+
+window.addEventListener("keydown", filterKeys);
+calSec.addEventListener("click", filterClick);
+
+function filterClick(event) {  
+
+    const inputNum = document.querySelector("#number-input");
+    const opSymbol = document.querySelector("#op-symbol");    
+    const btnText = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+    const btnNames = ["add", "subtract", "multiply", "divide"];
+
+    if (btnText.includes(event.target.innerText)) displayNumbers(event, inputNum);
+
+    if (event.target.name === "Enter" && opSymbol != "") calculateValues(inputNum, opSymbol);
+
+    if (event.target.name === "Clear") clearDisplay(inputNum, opSymbol);    
+    
+    if (btnNames.includes(event.target.name)) contCalculation(event, inputNum, opSymbol);           
+}    
+
+function filterKeys(event) {
+
+    const inputNum = document.querySelector("#number-input");
+    const opSymbol = document.querySelector("#op-symbol");
+    const numKeys = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+    const keys = ["+", "-", "*", "x", "/"]; 
+
+    if (numKeys.includes(event.key)) displayNumbers(event, inputNum);
+
+    if (event.key === "Enter" && opSymbol != "") calculateValues(inputNum, opSymbol);    
+
+    if (event.key === "Clear") clearDisplay(inputNum, opSymbol); 
+
+    if (keys.includes(event.key)) contCalculation(event, inputNum, opSymbol);
+}   
+
+function contCalculation(event, elementNum, elementOP) {
+    elementOP.innerText === "" ? (   
+        displayOperator(event, elementOP),
+        saveDisplayedValue(elementNum)
+    ) : (
+        calculateValues(elementNum, elementOP),
+        clearDisplay(elementNum, elementOP),
+        displayOperator(event, elementOP)
+    );
+}
+
+function clearDisplay(elementNum, elementOP) {
+    elementNum.value || elementOP.innerText ? (
+        elementNum.value = "",
+        elementOP.innerText = ""
+    ) :  numMemory = 0;
+}
+
+function saveDisplayedValue(elementNum) {
+    numMemory = Number(elementNum.value);
+    elementNum.value = "";
+}
+
+function calculateValues(elementNum, elementOP) {
+    let numInput = Number(elementNum.value);
+    numMemory = operate(elementOP.name, numMemory, numInput);
+    elementNum.value = numMemory;
+    elementOP.innerText = "";        
+}
+
+function displayOperator(event, element) {
+    event.type === "click" ? (
+        element.innerText = event.target.id,
+        element.name = event.target.name
+    ) : (
+        element.innerText = event.key,
+        findName(event, element)
+    );
+}
+
+function displayNumbers(event, element) {
+    element.value += event.type === "click" ? event.target.innerText : event.key;
+}
+
+function findName(event, element) {
+    let ekey = event.key;
+    if (ekey === "+") element.name = "add";  
+    if (ekey === "-") element.name = "subtract";
+    if (ekey === "x") element.name = "multiply";
+    if (ekey === "*") element.name = "multiply";
+    if (ekey === "/") element.name = "divide";   
+}
+
+function operate(operation, total, number) {
+    if (operation === "add") return add(total, number);
+    if (operation === "subtract") return subtract(total, number);
+    if (operation === "multiply") return  multiply(total, number);
+    if (operation === "divide") return divide(total, number);
+}
+
 function add(total, number) {
     return total += number;  
 }
@@ -14,19 +113,5 @@ function divide(total, number) {
     return total /= number;
 }
 
-function operate(operation, total, number) {
-    if (operation === "add") { 
-        return add(total, number);
-    };
-    if (operation === "subtract") { 
-        return subtract(total, number);
-    };
-    if (operation === "multiply") { 
-        return  multiply(total, number);
-    };
-    if (operation === "divide") { 
-        return divide(total, number);
-    };
-   
-}
+
 
